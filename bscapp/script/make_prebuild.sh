33a3b2120260b949d283a1923608d4059d3c6f0d
Librae <librae8226@gmail.com>
2015-09-17 12:55:06 +0800

    bsc: add prebuilt binary

---
+++ bscapp/script/make_prebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -f "$1" ] || [ ! -d "$2" ]; then
+	echo $0 'src_bin dst_dir [release_type]'
+	exit 1
+fi
+
+src_bin=$1
+dst_dir=$2
+release_type=$3
+
+if [ ! hit$release_type = 'hitrelease' ]; then
+	release_type='test'
+fi
+
+cp -v $src_bin $dst_dir/nuttx_bscapp_g`git show --pretty="%h" | head -n 1`_`date +%Y%m%d`.`echo $release_type`.bin
